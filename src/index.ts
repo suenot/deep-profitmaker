a1aafb8624b693ef3997ab0b91f2ff652717da11
Eugen Soloviov <suenot@gmail.com>
2022-08-16 23:11:52 +0000

    some types

--- src/index.ts
+++ src/index.ts
@@ -1,181 +1,283 @@
-require('dotenv').config();
+require('react');
+require('graphql');
+require('lodash');
+require('subscriptions-transport-ws');
 
-// import * as Sentry from '@sentry/node';
-// import * as Tracing from '@sentry/tracing';
-import bodyParser from 'body-parser';
-import Debug from 'debug';
-import express from 'express';
-import gql from 'graphql-tag';
-import _ from 'lodash';
-// import 'source-map-support/register';
-import { generateApolloClient } from '../hasura';
-// import { middlewares } from './integrations/tinkoff-split/middlewares';
-// import { paymentStageHandler } from './integrations/tinkoff-split/payment-stage-handler';
-// import { sharingProviderStageHandler } from './integrations/tinkoff-split/sharing_provider-stage-handler';
-// import { usingStageHandler } from './integrations/tinkoff-split/using-stage-handler';
+const { generateApolloClient } = require("@deep-foundation/hasura/client");
+const { DeepClient } = require('@deep-foundation/deeplinks/imports/client');
+const { minilinks, Link } = require('@deep-foundation/deeplinks/imports/minilinks');
 
-const PORT_ENV = process.env.PORT;
+const apolloClient = generateApolloClient({
+  path: process.env.NEXT_PUBLIC_GQL_PATH || '', // <<= HERE PATH TO UPDATE
+  ssl: !!~process.env.NEXT_PUBLIC_GQL_PATH.indexOf('localhost') ? false : true,
+  // admin token in prealpha deep secret key
+  // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsibGluayJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJsaW5rIiwieC1oYXN1cmEtdXNlci1pZCI6IjI2MiJ9LCJpYXQiOjE2NTYxMzYyMTl9.dmyWwtQu9GLdS7ClSLxcXgQiKxmaG-JPDjQVxRXOpxs',
+});
 
-// const PAYMENT_QUERY = gql`query ($paymentId: Int, $nodeId: String) { payments(where: {id: {_eq: $paymentId}, nodeId: {_eq: $nodeId} }) { id nodeId } }`;
+const unloginedDeep = new DeepClient({ apolloClient });
 
-// const DELETE_PAYMENT = gql`mutation DeleteNotification($paymentId: Int) { delete_payments(where: {id: {_eq: $paymentId}}) { returning { id } } }`;
+const delay = (time = 1000) => new Promise(res => setTimeout(res, time));
 
-// const INSERT_JSON = gql`mutation insertJson($paymentId: String, $json: jsonb) { update_nodes(where: {id: {_eq: $paymentId}}, _set: {json: $json}) { returning { id } } }`;
-// const APPEND_JSON = gql`mutation insertJson($paymentId: String, $json: jsonb) { update_nodes(where: {id: {_eq: $paymentId}}, _append: {json: $json}) { returning { id } } }`;
+const f = async () => {
+  const guest = await unloginedDeep.guest();
+  const guestDeep = new DeepClient({ deep: unloginedDeep, ...guest });
+  const admin = await guestDeep.login({ linkId: await guestDeep.id('deep', 'admin') });
+  const deep = new DeepClient({ deep: guestDeep, ...admin });
 
-// const NODE_QUERY = gql`query ($nodeId: String) {
-//   nodes(where: {
-//     id: {_eq: $nodeId}
-//   }) { id source_id target_id key type format string number boolean json date point path }
-// }`;
+  const User = await deep.id('@deep-foundation/core', 'User');
+  const Type = await deep.id('@deep-foundation/core', 'Type');
+  const Any = await deep.id('@deep-foundation/core', 'Any');
+  const Join = await deep.id('@deep-foundation/core', 'Join');
+  const Contain = await deep.id('@deep-foundation/core', 'Contain');
+  const Value = await deep.id('@deep-foundation/core', 'Value');
+  const String = await deep.id('@deep-foundation/core', 'String');
+  const Package = await deep.id('@deep-foundation/core', 'Package');
 
-const debug = Debug('server:index');
+  const SyncTextFile = await deep.id('@deep-foundation/core', 'SyncTextFile');
+  const dockerSupportsJs = await deep.id('@deep-foundation/core', 'dockerSupportsJs');
+  const Handler = await deep.id('@deep-foundation/core', 'Handler');
+  const HandleInsert = await deep.id('@deep-foundation/core', 'HandleInsert');
+  const HandleDelete = await deep.id('@deep-foundation/core', 'HandleDelete');
 
-// export const setStage = async (nodeId, stage, client) => {
-//   debug('setStage', { nodeId, stage, client: typeof client });
-//   console.trace('setStage');
-//   if (!nodeId) return { error: '!nodeId' };
-//   if (!stage) return { error: '!stage' };
-//   return await client.mutate({ mutation: UPDATE_STAGE, variables: { nodeId, stage } });
-// };
+  const Tree = await deep.id('@deep-foundation/core', 'Tree');
+  const TreeIncludeNode = await deep.id('@deep-foundation/core', 'TreeIncludeNode');
+  const TreeIncludeUp = await deep.id('@deep-foundation/core', 'TreeIncludeUp');
+  const TreeIncludeFromCurrent = await deep.id('@deep-foundation/core', 'TreeIncludeFromCurrent');
 
-// export const setError = async (paymentNode, response, client) => {
-//   debug('setError', { paymentNode, response, client: typeof client });
-//   if (!paymentNode.id) return { error: '!paymentNode.id' };
-//   if (!paymentNode.json) {
-//     await client.mutate({
-//       mutation: INSERT_JSON,
-//       variables: {
-//         paymentId: paymentNode.id,
-//         json: { pay: { type: 'tinkoff', response } },
-//       },
-//     });
-//   } else if (paymentNode.json && !paymentNode.json.pay) {
-//     await client.mutate({
-//       mutation: APPEND_JSON,
-//       variables: {
-//         paymentId: paymentNode.id,
-//         json: { pay: { type: 'tinkoff', response } },
-//       },
-//     });
-//   } else if (paymentNode.json && paymentNode.json.pay) {
-//     const newJson = _.cloneDeep(paymentNode.json);
-//     newJson.pay.response = response;
-//     debug('setError newJson', JSON.stringify(newJson));
-//     await client.mutate({
-//       mutation: INSERT_JSON,
-//       variables: {
-//         paymentId: paymentNode.id,
-//         json: newJson,
-//       },
-//     });
-//   }
-//   await client.mutate({ mutation: UPDATE_STAGE, variables: { nodeId: paymentNode.id, stage: 'rejected' } });
-// };
+  const Rule = await deep.id('@deep-foundation/core', 'Rule');
+  const RuleSubject = await deep.id('@deep-foundation/core', 'RuleSubject');
+  const RuleObject = await deep.id('@deep-foundation/core', 'RuleObject');
+  const RuleAction = await deep.id('@deep-foundation/core', 'RuleAction');
+  const Selector = await deep.id('@deep-foundation/core', 'Selector');
+  const SelectorInclude = await deep.id('@deep-foundation/core', 'SelectorInclude');
+  const SelectorExclude = await deep.id('@deep-foundation/core', 'SelectorExclude');
+  const SelectorTree = await deep.id('@deep-foundation/core', 'SelectorTree');
+  const containTree = await deep.id('@deep-foundation/core', 'containTree');
+  const AllowInsertType = await deep.id('@deep-foundation/core', 'AllowInsertType');
+  const AllowDeleteType = await deep.id('@deep-foundation/core', 'AllowDeleteType');
+  const SelectorFilter = await deep.id('@deep-foundation/core', 'SelectorFilter');
+  const Query = await deep.id('@deep-foundation/core', 'Query');
+  const usersId = await deep.id('deep', 'users');
 
-export const initialize = (
-  app = express(),
-  client = generateApolloClient(),
-  onRequest?,
-  onListen?,
-) => {
-
-  // Sentry.init({
-  //   dsn: process.env.SENTRY_DSN,
-  //   maxBreadcrumbs: 50,
-  //   debug: true,
-  //   release,
-  //   integrations: [
-  //     new Sentry.Integrations.Http({ tracing: true }),
-  //     new Tracing.Integrations.Express({
-  //       app,
-  //     }),
-  //   ]});
-  // app.use(Sentry.Handlers.requestHandler());
-  // app.get('/debug-sentry', (req, res) => {
-  //   throw new Error('Sentry test!');
-  // });
-
-  // const deletePayment = async (id) => {
-  //   if (!id) return { error: '!id' };
-  //   return await client.mutate({
-  //     mutation: DELETE_PAYMENT,
-  //     variables: { paymentId: id },
-  //   });
-  // };
-
-  app.get('/healthz', (req, res) => res.send({ status: 'ok' }));
-  app.use(bodyParser.json());
-
-  // middlewares({ app, client });
-  // app.post('/', async (req, res) => {
-  //   if (onRequest) await onRequest(req, res);
-  //   debug('request /', req.body);
-
-  //   const { id, nodeId } = _.get(req, 'body.event.data.new', {});
-  //   debug('input', { id, nodeId });
-
-  //   if (!id) await res.json({ error: '!id' });
-  //   if (!nodeId) await res.json({ error: '!nodeId' });
-
-  //   const paymentRow = _.get(
-  //     await client.query({
-  //       query: PAYMENT_QUERY,
-  //       variables: {
-  //         paymentId: +id, nodeId,
-  //       },
-  //     }),
-  //     'data.payments.0',
-  //   );
-
-  //   if (!paymentRow) {
-  //     debug('illegal', { id, nodeId });
-  //     await deletePayment(id);
-  //     return await res.json({ error: 'illegal' });
-  //   }
-
-  //   const inputNode = _.get(
-  //     await client.query({
-  //       query: NODE_QUERY,
-  //       variables: { nodeId },
-  //     }),
-  //     'data.nodes.0',
-  //   );
-
-  //   if (!inputNode) {
-  //     debug('!node', { id, nodeId });
-  //     await deletePayment(id);
-  //     return await res.json({ error: '!node' });
-  //   }
-
-  //   const stageNode = _.get(
-  //     await client.query({
-  //       query: STAGE_BY_NODE,
-  //       variables: { nodeId: inputNode.id },
-  //     }),
-  //     'data.nodes.0',
-  //   );
-
-  //   debug('loaded', { inputNode, stageNode });
-
-  //   // const result = await handleNode({ inputNode, client });
-  //   let result;
-  //   if (inputNode?.type === 'payment') {
-  //     result = await paymentStageHandler({ inputNode, stageNode, client });
-  //   } else if (inputNode?.type === 'using') {
-  //     result = await usingStageHandler({ inputNode, stageNode, client });
-  //   } else if (inputNode?.type === 'sharing_provider') {
-  //     result = await sharingProviderStageHandler({ inputNode, stageNode, client });
-  //   }
-  //   await res.json(result || { error: 'unexpected' });
-
-  //   debug('deletePayment', { id, nodeId });
-  //   await deletePayment(id);
-  // });
-
-  app.listen(PORT_ENV, () => {
-    console.log(`Listening on port ${PORT_ENV}!`);
-    console.log('Hello bugfixers!');
-    if (onListen) onListen(app);
+  const { data: [{ id: packageId }] } = await deep.insert({
+    type_id: Package,
+    string: { data: { value: `@deep-foundation/profitmaker` } },
+    in: { data: [
+      {
+        type_id: Contain,
+        from_id: deep.linkId
+      },
+    ] },
+    out: { data: [
+      {
+        type_id: Join,
+        to_id: await deep.id('deep', 'users', 'packages'),
+      },
+      {
+        type_id: Join,
+        to_id: await deep.id('deep', 'admin'),
+      },
+    ] },
   });
+  console.log({ packageId });
+
+  // INVEST PROVIDER
+  const { data: [{ id: BrokerProvider }] } = await deep.insert({
+    type_id: Type,
+    from_id: Any,
+    to_id: Any,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'BrokerProvider' } },
+    } },
+  });
+  console.log({ BrokerProvider });
+
+  // ASSET
+  const { data: [{ id: Asset }] } = await deep.insert({
+    type_id: Type,
+    from_id: Any,
+    to_id: Any,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Asset' } },
+    } },
+  });
+  console.log({ Asset });
+
+  const { data: [{ id: Isin }] } = await deep.insert({
+    type_id: Type,
+    from_id: BrokerProvider,
+    to_id: Asset,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Isin' } },
+    } },
+  });
+  console.log({ Isin });
+
+  const { data: [{ id: AssetTicker }] } = await deep.insert({
+    type_id: Type,
+    from_id: BrokerProvider,
+    to_id: Asset,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'AssetTicker' } },
+    } },
+  });
+  console.log({ AssetTicker });
+
+  const { data: [{ id: AssetData }] } = await deep.insert({
+    type_id: Type,
+    from_id: BrokerProvider,
+    to_id: Asset,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'AssetData' } },
+    } },
+  });
+  console.log({ AssetData });
+
+  // INSTRUMENT
+  const { data: [{ id: Instrument }] } = await deep.insert({
+    type_id: Type,
+    from_id: Any,
+    to_id: Any,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Instrument' } },
+    } },
+  });
+  console.log({ Instrument });
+
+  const { data: [{ id: Figi }] } = await deep.insert({
+    type_id: Type,
+    from_id: BrokerProvider,
+    to_id: Instrument,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Figi' } },
+    } },
+  });
+  console.log({ Figi });
+
+  const { data: [{ id: InstrumentTicker }] } = await deep.insert({
+    type_id: Type,
+    from_id: BrokerProvider,
+    to_id: Asset,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'InstrumentTicker' } },
+    } },
+  });
+  console.log({ InstrumentTicker });
+
+  const { data: [{ id: InstrumentData }] } = await deep.insert({
+    type_id: Type,
+    from_id: BrokerProvider,
+    to_id: Instrument,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'InstrumentData' } },
+    } },
+  });
+  console.log({ InstrumentData });
+
+  // WALLET
+  const { data: [{ id: Wallet }] } = await deep.insert({
+    type_id: Type,
+    from_id: User,
+    to_id: BrokerProvider,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Wallet' } },
+    } },
+  });
+  console.log({ Wallet });
+
+  // TRANSACTION
+  const { data: [{ id: Transaction }] } = await deep.insert({
+    type_id: Type,
+    from_id: Wallet,
+    to_id: Wallet,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Transaction' } },
+    } },
+  });
+  console.log({ Transaction });
+
+  const { data: [{ id: Base }] } = await deep.insert({
+    type_id: Type,
+    from_id: Any, // Transaction
+    to_id: Any, // Asset
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Base' } },
+    } },
+  });
+  console.log({ Base });
+
+  const { data: [{ id: Quote }] } = await deep.insert({
+    type_id: Type,
+    from_id: Any, // Transaction
+    to_id: Any, // Asset
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Quote' } },
+    } },
+  });
+  console.log({ Quote });
+
+  // TRANSACTION: PRICE, AMOUNT, TOTAL
+  const { data: [{ id: Price }] } = await deep.insert({
+    type_id: Type,
+    from_id: Transaction,
+    to_id: Instrument,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Price' } },
+    } },
+  });
+  console.log({ Price });
+
+  const { data: [{ id: Amount }] } = await deep.insert({
+    type_id: Type,
+    from_id: Transaction,
+    to_id: Instrument,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Amount' } },
+    } },
+  });
+  console.log({ Amount });
+
+  const { data: [{ id: Total }] } = await deep.insert({
+    type_id: Type,
+    from_id: Amount,
+    to_id: Price,
+    in: { data: {
+      type_id: Contain,
+      from_id: packageId,
+      string: { data: { value: 'Total' } },
+    } },
+  });
+  console.log({ Total });
+
 };
+f();
